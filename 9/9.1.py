dc6990448d15fbe0071b02484af24de1c447a6a1
Visa Lehtimäki <642229+ramla@users.noreply.github.com>
2024-12-10 15:08:57 +0200

    test checksum pass, production fails

--- 9/9.1.py
+++ 9/9.1.py
@@ -18,13 +18,22 @@ class defragger:
     def __init__(self):
         self.data = getinput(9, production = False)
         self.data = list(map(int,self.data))
-        self.data_copy = self.data[:]
-        self.size = len(self.data)
+        self.write_fileids(self.data)
+        #self.i_still_needed_the_block_representation()
+        #print(" ".join(list(map(str,self.blocks))))
         self.first_free_index = 1
         self.process_data()
         self.i_still_needed_the_block_representation()
         self.checksum()
-    
+
+    def write_fileids(self, data):
+        i = 0
+        for item in data:
+            if i%2 == 0:
+                data[i] = (item,i//2)
+            i += 1
+        #print(self.data)
+
     def reset_file_iterator(self):
         return len(self.data)-1
 
@@ -43,10 +52,10 @@ class defragger:
         free = self.reset_free_iterator()
         
         while True:
-            print("free index:",free,"space:",self.data[free],"file index:",file//2,"filesize:",self.data[file])
+            #print("free index:",free,"space:",self.data[free],"file index:",file//2,"filesize:",self.data[file])
             filecandidate = self.data[file]
             freecandidate = self.data[free]
-            if filecandidate > freecandidate:
+            if filecandidate[0] > freecandidate:
                 if free < file-2:
                     free += 2
                     freecandidate = self.data[free]
@@ -57,37 +66,41 @@ class defragger:
                         file -= 2
                         filecandidate = self.data[file]
                         free = self.reset_free_iterator()
-                        print("no space for whole file")
+                        #print("no space for whole file")
                     else:
                         
                         break
             else:
                 # data up to file before free space + now empty space between files + copy file into new location + remaining free space + everything after
-                self.data = self.data[:free] + [0] + [self.data[file]] + [freecandidate - filecandidate] + self.data[free+1:]
+                self.data = self.data[:free] + [0] + [self.data[file]] + [freecandidate - filecandidate[0]] + self.data[free+1:]
                 self.data.pop(file+2) #remove copied file
                 if file+2 != len(self.data):
-                    self.data[file+1] = self.data[file+1] + filecandidate + self.data.pop(file+2) #combine free space at and around removed file
+                    self.data[file+1] = self.data[file+1] + filecandidate[0] + self.data.pop(file+2) #combine free space at and around removed file
                 else:
                     self.data.pop(file+1) #remove trailing free space
-                print(self.data)
+                #print(self.data)
 
     def i_still_needed_the_block_representation(self):
+        #print(self.data)       
         self.blocks = []
-        fileid = 0
         blockcounter = 0
         i = 0
-        for val in self.data:
+        for object in self.data:
+            try:
+                val = object[0]
+            except:
+                val = object
             if i%2 == 0:
+                fileid = object[1]
                 for block in range(0,val):
                     self.blocks.append(fileid)
                 blockcounter += val
-                fileid += 1
             else:
                 for block in range(0,val):
-                    self.blocks.append(-1)
+                    self.blocks.append(0)
                 blockcounter += val
             i += 1
-        print("".join(list(map(str,self.blocks)))) #fuck how I still needed the block representation
+        print(" ".join(list(map(str,self.blocks))))
 
     def checksum(self):
         checksum = 0
